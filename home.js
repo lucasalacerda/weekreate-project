c7f01762e07121203fdad14179c0b4aa7a9bff6c
Lucas <lucasalvlacerda@gmail.com>
2018-11-21 01:23:03 -0200

    adiciona nova pagina de atividades - pagina que contem todas as atividades do evento detalhadamente; remove paginas separadas; adiciona estilo de nova pagina de atividades

--- home.js
+++ home.js
@@ -1,31 +1,47 @@
 function changeBackgroundPalestras() {
     console.log('palestras');
-    var main = document.querySelector('main');
+    var main = document.querySelector('div#main');
     main.classList.add('background-palestras');
 }
 
 function changeBackgroundWorkshops() {
     console.log('workshops');
-    var main = document.querySelector('main');
+    var main = document.querySelector("div#main");
     main.classList.add('background-workshops');
 }
 
 function changeBackgroundDinamica() {
     console.log('dinamica');
-    var main = document.querySelector('main');
+    var main = document.querySelector('div#main');
     main.classList.add('background-dinamica');
 }
 
 function changeBackgroundMais() {
     console.log('mais');
-    var main = document.querySelector('main');
+    var main = document.querySelector('div#main');
     main.classList.add('background-mais');
 }
 
 function clearBackground() {
     console.log('remove');
-    var main = document.querySelector('main');
-    main.classList.add('.main');
+    var main = document.querySelector("div#main");
     main.classList.remove('background-workshops', 'background-palestras', 'background-dinamica', 'background-mais');
+}
+
+function onMouseOver(e) {
+    var timeout;
+    e.preventDefault();
+
+    var list = document.querySelector('#lista');
+    var meioLista = (window.innerWidth / 2);
+    var x = e.clientX;
+
+    if (x < (meioLista)) {
+        list.scroll(-4000, 0);
+
+    } else if (x > (meioLista)) {
+        list.scroll(4000, 0);
+    }
+
 }
 
